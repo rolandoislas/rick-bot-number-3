9c7a5c095ae4eee36b0990b8b8f92609e9ab5114
Rolando Islas <admin@rolandoislas.com>
2017-04-02 05:32:47 -0700

    Add countdown with proper timezone

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
+import os
+
 NAME = "Rick Bot Number 3"
-VERSION = "1.2.2"
+VERSION = "1.3"
 USERAGENT = "python:com.rolandoislas.rickbotnumberthree:v%s (by /u/rolandoislas)" % VERSION
 REPO = "https://github.com/rolandoislas/rick-bot-number-3"
-SEASON_3_URL = "http://www.adultswim.com/videos/streams"
+SEASON_3_URL = os.environ.get("SEASON_THREE_URL")
